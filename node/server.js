5dfa15a7abd7988dd6550326d5492b1ef2f69cd8
Jadson Medeiros <developer.medeiros@gmail.com>
2022-08-01 23:43:16 -0300

    added docker

--- node/server.js
+++ node/server.js
@@ -1,7 +1,8 @@
 import app from './src/app.js';
 
 const port = process.env.PORT || 3000;
+const host = '0.0.0.0';
 
-app.listen(port, () => {
-  console.log('listening on port ' + port);
+app.listen(port, host, () => {
+  console.log('listening on port ' + port + ' and host ' + host)
 })
