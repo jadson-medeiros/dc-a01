e75583ac900cc20e6f4b0630f20474207ce18c30
Jadson Medeiros <developer.medeiros@gmail.com>
2022-08-04 16:17:36 -0300

    updated gui

--- gui/main.js
+++ gui/main.js
@@ -1,44 +1,62 @@
-function addItem() {
-  let data = document.getElementById("text-input").value
-  let url = "http://127.0.0.1:8000/tb01"
-
-  fetch(url, {
-      method: "POST",
-      body: JSON.stringify({text: data}),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-  }).then(() => loadItems())
+function loadItems() {
+    let node_url = "http://127.0.0.1:3000/tb01"
+    let data = getItems(node_url);
+    let items = JSON.parse(data);
+    let table = document.getElementById("table");
+    items.forEach(element => {
+        let row = getRow(element)
+        table.appendChild(row);
+    });
 }
 
-function loadItems() {
+function addItem() {
+    preventDefault()
+    let go_url = "http://127.0.0.1:8000/tb01"
+    let col_text = document.getElementById("col_text").value
+    body = {
+        "col_text": col_text,
+    }
 
-  let raw = `<table>
-              <thead>
-              <th id="id">ID</th>
-              <th id="text">Text</th>
-              <th id="timestamp">Date</th>
-              </thead>
-          </table>`
+    let req = new XMLHttpRequest()
+    req.open("POST", go_url, true)
+    req.setRequestHeader("Content-type", "application/json")
+    req.setRequestHeader("Access-Control-Allow-Origin", "*")
 
-  let table = document.getElementsByTagName("table")[0]
-  table.innerHTML = raw
+    req.send(JSON.stringify(body))
 
-  fetch("http://127.0.0.1:3000/tb01")
-      .then(res => res.json())
-      .then(res => res.forEach((row, i) => {
-          if ((i % 2) === 0) {
-              table.innerHTML = table.innerHTML +
-                  `<tr class="even">
-                      <td>${row.id}</td>
-                      <td>${row.text}</td>
-                      <td>${row.date}</td>
-                  </tr>`
-          } else {
-              table.innerHTML = table.innerHTML +
-                  `<tr class="odd">
-                      <td>${row.id}</td>
-                      <td>${row.text}</td>
-                      <td>${row.date}</td>
-                  </tr>`
-          }
-      }))
+    req.onload = function() {
+        console.log(this.responseText)
+    }
+
+    return req.responseText
+}
+
+
+function getItems(url){
+    let req = new XMLHttpRequest()
+    try {
+        req.setRequestHeader("Access-Control-Allow-Origin", "*")
+        req.open("GET", url, false)
+        req.send()
+        return req.responseText
+    } catch (err) {
+        console.log(err)
+    }
+}
+
+function getRow(element) {
+    row = document.createElement("tr");
+    tdId = document.createElement("td");
+    tdText = document.createElement("td");
+    tdDate = document.createElement("td");
+
+    tdId.innerHTML = element.id
+    tdText.innerHTML = element.col_text
+    tdDate.innerHTML = `${element.col_dt.substring(0,10)} ${element.col_dt.substring(11,19)}`
+
+    row.appendChild(tdId);
+    row.appendChild(tdText);
+    row.appendChild(tdDate);
+
+    return row;
 }
